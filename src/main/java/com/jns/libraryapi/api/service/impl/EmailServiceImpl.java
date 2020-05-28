8691be63be1427622bd9e9723040f8374f8e145f
jairosousa <jaironsousa@gmail.com>
2020-05-28 16:37:16 -0300

    Implementando o serviço de email

--- src/main/java/com/jns/libraryapi/api/service/impl/EmailServiceImpl.java
+++ src/main/java/com/jns/libraryapi/api/service/impl/EmailServiceImpl.java
@@ -1,13 +1,33 @@
 package com.jns.libraryapi.api.service.impl;
 
 import com.jns.libraryapi.api.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${application.mail.default.remetent}")
+    private String remetent;
+
+    private final JavaMailSender javaMailSender;
 
     @Override
     public void sendMails(String message, List<String> emails) {
+        String[] mails = emails.toArray(new String[emails.size()]);
 
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(remetent);
+        mailMessage.setSubject("Livro com emprestimo atrasado");
+        mailMessage.setText(message);
+        mailMessage.setTo(mails);
+
+        javaMailSender.send(mailMessage);
     }
 }
