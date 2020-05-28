28b775e93b1317c03e8d3c8e67f0dac5c2b22142
jairosousa <jaironsousa@gmail.com>
2020-05-28 12:11:14 -0300

    Agendamento de tarefas com Schedule do Spring

--- src/main/java/com/jns/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/jns/libraryapi/LibraryApiApplication.java
@@ -4,8 +4,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibraryApiApplication {
 
     @Bean
@@ -15,6 +18,11 @@ public class LibraryApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApiApplication.class, args);
+    }
+
+    @Scheduled(cron = "0 10 12 1/1 * ?")
+    public void testeAgendamentoService() {
+        System.out.println("AGENDAMEMTO DE TAREFAS");
     }
 
 }
