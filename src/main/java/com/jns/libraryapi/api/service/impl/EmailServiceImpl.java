89eabf92bf03da00a2169b832ed9f775bf23552d
jairosousa <jaironsousa@gmail.com>
2020-05-28 16:27:30 -0300

    Configurando o servidor de email MailTrap para envio de emails

---
+++ src/main/java/com/jns/libraryapi/api/service/impl/EmailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.jns.libraryapi.api.service.impl;
+
+import com.jns.libraryapi.api.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendMails(String message, List<String> emails) {
+
+    }
+}
