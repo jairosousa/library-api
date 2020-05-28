ceadb7e231fcc039d253d37ed1184d61b6549261
jairosousa <jaironsousa@gmail.com>
2020-05-28 16:52:08 -0300

    Testando serviços de email

--- src/main/java/com/jns/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/jns/libraryapi/LibraryApiApplication.java
@@ -1,15 +1,33 @@
 package com.jns.libraryapi;
 
+import com.jns.libraryapi.api.service.EmailService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            List<String> emails = Arrays.asList("jnslibrary-api-694e6b@inbox.mailtrap.io");
+            emailService.sendMails("Testando serviços de emails", emails);
+            System.out.println("EMAILS ENVADOS");
+        };
+    }
 
     @Bean
     public ModelMapper modelMapper(){
